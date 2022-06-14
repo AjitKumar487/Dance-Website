dc120e5f6909f7ab0bd601368c4ef74f93f67078
Ajit Kumar <ajitku425@gmail.com>
2022-06-14 10:01:40 +0530

    Dance Website

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server started successfully on port ${port}`);
+})
